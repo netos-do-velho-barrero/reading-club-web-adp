b9869ae16a033b48fd2dfd7036ab236fd797bda1
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-05-27 15:01:11 -0300

    feat: implement Magazine ViewModels

---
+++ ClubeDaLeituraWeb.WebApp/ModuloRevista/Apresentacao/RevistaViewModels.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ClubeDaLeituraWeb.WebApp.ModuloRevista.Apresentacao;
+
+public record ListarRevistasViewModel(
+    string Id,
+    string Titulo,
+    int NumeroEdicao,
+    int AnoPublicacao,
+    string NomeCaixa
+);
+
+public record CadastrarRevistasViewModel(
+    [Required(ErrorMessage = "O campo \"Título\" deve ser preenchido.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O campo \"Título\" deve conter entre 2 e 100 caracteres.")]
+    string Titulo,
+
+    [Range(0, int.MaxValue, ErrorMessage = "O campo \"Número da Edição\" deve conter um valor igual ou maior que 0.")]
+    int NumeroEdicao,
+
+    [Range(1, 9999, ErrorMessage = "O campo \"Ano de Publicação\" deve conter uma data válida.")]
+    int AnoPublicacao,
+
+    [Required(ErrorMessage = "O campo \"Caixa\" deve ser preenchido.")]
+    string CaixaId
+);
+
+public record EditarRevistasViewModel(
+    string Id,
+
+    [Required(ErrorMessage = "O campo \"Título\" deve ser preenchido.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O campo \"Título\" deve conter entre 2 e 100 caracteres.")]
+    string Titulo,
+
+    [Range(0, int.MaxValue, ErrorMessage = "O campo \"Número da Edição\" deve conter um valor igual ou maior que 0.")]
+    int NumeroEdicao,
+
+    [Range(1, 9999, ErrorMessage = "O campo \"Ano de Publicação\" deve conter uma data válida.")]
+    int AnoPublicacao,
+
+    [Required(ErrorMessage = "O campo \"Caixa\" deve ser preenchido.")]
+    string CaixaId
+);
+
+public record ExcluirRevistasViewModel(
+    string Id,
+    string Titulo,
+    int NumeroEdicao,
+    int AnoPublicacao,
+    string NomeCaixa
+);
